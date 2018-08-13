4f5ad1d5c86de410ea10c1959df3bbbea5c1dbfc
toddland <tlandstad@hotmail.com>
2018-08-13 15:44:19 -0700

    Added HTML
    Added HTML

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data)
+{
+  if(error){
+    res.write('File Note Found');
+  }
+  else{
+    res.write(data);
+  }
+  res.end();
+})
+
 });
 
 module.exports = router;
