c5fbf7e59a30580e14d07df10b906321322c755c
toddland <tlandstad@hotmail.com>
2018-08-17 16:10:33 -0600

    Added logo

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.writeHead(200,{'Content-Type': 'text/html'});
-  fs.readFile('../index.html', null, function(error, data)
+  fs.readFile('./index.html', null, function(error, data)
 {
   if(error){
     res.write('File Note Found');
